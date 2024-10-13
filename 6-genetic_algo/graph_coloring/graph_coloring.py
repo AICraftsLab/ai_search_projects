4f760c51d19504aadca508ed8b24286921d3e2d9
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2024-10-13 07:47:38 +0100

    rank seltion`

--- 6-genetic_algo/graph_coloring/graph_coloring.py
+++ 6-genetic_algo/graph_coloring/graph_coloring.py
@@ -5,6 +5,7 @@ class Graph:
     def __init__(self, graph_dict, chromatic_number):
         self.chromatic_number = chromatic_number
         self.graph_dict = graph_dict
+        self.vertices = len(self.graph_dict)
 
 
 class Genome:
@@ -27,5 +28,32 @@ class Genome:
             if random.random() < prob:
                 self.chromosome[i] = random.choice(allele)
 
+    @classmethod
+    def reproduce(cls, parent1, parent2):
+        locus = random.randrange(len(parent1.chromosome))
+
+        chromosome1 = parent1.chromosome[:locus] + parent2.chromosome[locus:]
+        chromosome2 = parent2.chromosome[:locus] + parent1.chromosome[locus:]
+
+        offspring1 = Genome(chromosome1)
+        offspring2 = Genome(chromosome2)
+
+        return offspring1, offspring2
+
     def __repr__(self):
         return str(self.chromosome)
+
+
+class Population:
+    def __init__(self, size, graph):
+        self.size = size
+        self.graph = graph
+        self.members = []
+
+    def _initialize(self):
+        for i in range(self.size):
+            chromosome = [random.randrange(self.graph.chromatic_number) for x in range(self.graph.vertices)]
+            self.members.append(Genome(chromosome))
+
+    def _select_best(self, n):
+        pass
