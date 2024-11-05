148501333fdfba043ad0836bfb6e7363af2fe6f4
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2024-11-05 08:18:05 +0100

    knapsack docs

--- 6-genetic_algo/Knapsack2.py
+++ 6-genetic_algo/Knapsack2.py
@@ -1,16 +1,50 @@
 import random
 import matplotlib.pyplot as plt
 
-
+# Problem global variables
 GENERATIONS = 500
 POPULATION = 100
-TOTAL_ITEMS = 20
+TOTAL_ITEMS = 30
 BAG_CAPACITY = 1000
 MUTATION_PROB = 0.01
 ELITISM = 10
 
 
+HIKING_ITEMS = (
+    ("Laptop", 95, 150),
+    ("Smartphone", 80, 50),
+    ("Headphones", 40, 20),
+    ("Camera", 60, 120),
+    ("Watch", 30, 15),
+    ("Tablet", 70, 100),
+    ("Charger", 15, 10),
+    ("Speaker", 45, 80),
+    ("Notebook", 20, 10),
+    ("Shoes", 50, 100),
+    ("Jacket", 75, 180),
+    ("Sunglasses", 25, 30),
+    ("Water Bottle", 35, 50),
+    ("Tent", 90, 200),
+    ("Sleeping Bag", 65, 180),
+    ("Camping Stove", 55, 150),
+    ("Flashlight", 15, 25),
+    ("Map", 10, 10),
+    ("Gloves", 20, 15),
+    ("Compass", 25, 20),
+    ("Binoculars", 60, 100),
+    ("Hiking Boots", 85, 160),
+    ("Towel", 30, 50),
+    ("First Aid Kit", 50, 70),
+    ("Power Bank", 35, 60),
+    ("Hiking Pole", 40, 100),
+    ("Raincoat", 55, 120),
+    ("Snacks", 20, 30),
+    ("Notebook", 20, 20),
+)
+
+
 def plot_best_history(best_history, save_path=None):
+    """Func to plot history"""
     plt.figure()
     
     x_coord, values, weights = zip(*best_history)
@@ -33,11 +67,13 @@ def plot_best_history(best_history, save_path=None):
 
 
 def print_items(items):
+    """Func to print bag items info"""
     for item in items:
         print(item)
 
 
 class Item:
+    """Class to represent a single item"""
     def __init__(self, weight, value, name):
         self.weight = weight
         self.value = value
@@ -48,6 +84,7 @@ class Item:
         
     @classmethod
     def from_random_items(cls, n, min_value=10, max_value=100, min_weight=10, max_weight=BAG_CAPACITY // 5):
+        """Creates a random list of items"""
         items = []
         
         for i in range(n):
@@ -58,6 +95,17 @@ class Item:
             item = Item(weight, value, name)
             items.append(item)
             
+        return items
+        
+    @classmethod
+    def from_items_list(cls, items_list):
+        global TOTAL_ITEMS
+        items = []
+        for name, value, weight in items_list:
+            item = Item(weight, value, name)
+            items.append(item)
+            
+        TOTAL_ITEMS = len(items)
         return items
 
 
@@ -73,10 +121,11 @@ class Genome:
         for i in range(len(self.chromosome)):
             gene = self.chromosome[i]
             if gene == 1:
-                item = self.ITEMS[i]
+                item = self.ITEMS[i]  # getting item
                 fitness += item.value
                 total_weight += item.weight
         
+        # punish genomes for exceeding bag capacity
         if total_weight > BAG_CAPACITY:
             fitness = 0
             
@@ -88,7 +137,7 @@ class Genome:
         for i in range(len(self.chromosome)):
             gene = self.chromosome[i]
             if gene == 1:
-                item = self.ITEMS[i]
+                item = self.ITEMS[i]  # getting item
                 total_weight += item.weight
         
         return total_weight
@@ -99,13 +148,14 @@ class Genome:
                 self.chromosome[i] = int(not self.chromosome[i])
     
     def print_items(self):
+        """Prints items selected by this genome"""
         for i, gene in enumerate(self.chromosome):
             if gene == 1:
                 item = self.ITEMS[i]
                 print(item)
     
     @classmethod
-    def crossover(cls, genome1_x, genome2_x):
+    def _singlepoint_crossover(cls, genome1_x, genome2_x):
         crossover_point = random.randrange(TOTAL_ITEMS)
         child1_x = genome1_x[:crossover_point] + genome2_x[crossover_point:]
         child2_x = genome2_x[:crossover_point] + genome1_x[crossover_point:]
@@ -113,7 +163,7 @@ class Genome:
         return Genome(child1_x), Genome(child2_x)
         
     @classmethod
-    def crossover_2p(cls, genome1_x, genome2_x):
+    def _doublepoint_crossover(cls, genome1_x, genome2_x):
         points = random.sample(range(TOTAL_ITEMS), k=2)
         points.sort()
         point1, point2 = points
@@ -122,39 +172,49 @@ class Genome:
         
         return Genome(child1_x), Genome(child2_x)
         
+    @classmethod
+    def crossover(cls, genome1_x, genome2_x, c_type='single'):
+        if c_type == 'single':
+            return cls._singlepoint_crossover(genome1_x, genome2_x)
+        elif c_type == 'double':
+            return cls._doublepoint_crossover(genome1_x, genome2_x)
 
 class Population:
+    """Class to represent a population"""
     def __init__(self, size):
         self.size = size
         self.genomes = []
         self._initialize()
         
     def _initialize(self):
+        """Initialize population with random genomes"""
         for _ in range(self.size):
-            chromosome = [random.randrange(1) for _ in range(TOTAL_ITEMS)]
+            chromosome = [random.randrange(2) for _ in range(TOTAL_ITEMS)]
             genome = Genome(chromosome)
             self.genomes.append(genome)
     
     def generate_next_generation(self):
+        """Generates the next generation by evolution"""
         next_gen_genomes = []
         
-        parents_n = (POPULATION - ELITISM) * 2
-        parents = self._select_parents(parents_n)
+        parents_n = POPULATION - ELITISM
+        parents = self._select_parents(parents_n)  # selection
         
         for i in range(0, parents_n, 2):
             parent1 = parents[i]
             parent2 = parents[i + 1]
-            child1, child2 = Genome.crossover_2p(parent1.chromosome, parent2.chromosome)
+            # crossover & mutation
+            child1, child2 = Genome.crossover(parent1.chromosome, parent2.chromosome)
             child1.mutate(MUTATION_PROB)
             child2.mutate(MUTATION_PROB)
             next_gen_genomes.append(child1)
             next_gen_genomes.append(child2)
             
-        elites = self._get_elites(ELITISM)
+        elites = self._get_elites(ELITISM)  # elitism
         next_gen_genomes.extend(elites)
         self.genomes = next_gen_genomes
         
-        return elites[0]
+        return elites[0]  # return best genome, top elite
     
     def _get_elites(self, elitism):
         genomes = [(g, g.get_fitness()) for g in self.genomes]
@@ -181,12 +241,15 @@ if __name__ == '__main__':
     seed = None
     random.seed(seed)
     
-    population = Population(POPULATION)
-    items = Item.from_random_items(TOTAL_ITEMS)
+    #items = Item.from_random_items(TOTAL_ITEMS)
+    items = Item.from_items_list(HIKING_ITEMS)
     Genome.ITEMS = items
+    population = Population(POPULATION)
     
+    # keeping track of best
     overall_best = None
     overall_best_fitness = 0
+    overall_best_weight = 0
     best_history = []
     
     for i in range(GENERATIONS):
@@ -196,14 +259,16 @@ if __name__ == '__main__':
         if overall_best is None:
             overall_best = best
             overall_best_fitness = best_fitness
+            overall_best_weight = best.get_weight()
             best_history.append((i, best_fitness, best.get_weight()))
         else:
             if best_fitness > overall_best_fitness:
                 overall_best = best
                 overall_best_fitness = best_fitness
+                overall_best_weight = best.get_weight()
                 best_history.append((i, best_fitness, best.get_weight()))
                 
-        print('Generation:', i, 'Best Fitness:', overall_best_fitness, overall_best.chromosome)
+        print('Generation:', i, 'Best Fitness:', overall_best_fitness, 'Weight:', overall_best_weight)
     
     print('All items')
     print_items(items)
